56d99708d16d02832237641f6e74cafa78649ab9
charlesgodoy <charlesgodoy@live.com>
2023-10-23 12:20:07 -0400

    Added a local .mp4 file to the raw directory. Then added the functionality. Its pretty simple so I didn't bother cleaning it up, since its less of a user app, and more of practicing a proof of concept.

--- app/src/main/java/com/charlesgodoy/videoplayerapp/MainActivity.kt
+++ app/src/main/java/com/charlesgodoy/videoplayerapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.charlesgodoy.videoplayerapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // From local storage
+        val videoView1 : VideoView = findViewById(R.id.video_view_1)
+        videoView1.setVideoPath("android.resource://${this.packageName}/${R.raw.play_piano}")
+
+        // Media Controller 1 - Local
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView1)
+        mediaController.setMediaPlayer(videoView1)
+        videoView1.setMediaController(mediaController)
+        videoView1.start()   // enables it to start on startup
+
+
+
+        // From the internet
+        val videoVideo2 : VideoView = findViewById(R.id.video_view_2)
+        val uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/002/231/original/5226496.mp4")
+
+        videoVideo2.setVideoURI(uri)
+
+        // Media Controller 2 - Internet
+        val mediaController2 = MediaController(this)
+        mediaController2.setAnchorView(videoVideo2)
+        mediaController2.setMediaPlayer(videoVideo2)
+        videoVideo2.setMediaController(mediaController2)
+        videoVideo2.start()
+
     }
 }
